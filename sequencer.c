9577cb405f25cb73e2ffcdc288e51ae6ec4d19bf
Rohit Ashiwal <rohit.ashiwal265@gmail.com>
2019-05-30 05:30:27 +0530

    sequencer: add advice for revert
    In the case of merge conflicts, while performing a revert, we are
    currently advised to use `git cherry-pick --<sequencer-options>`
    of which --continue is incompatible for continuing the revert.
    Introduce a separate advice message for `git revert`.
    
    Signed-off-by: Rohit Ashiwal <rohit.ashiwal265@gmail.com>

--- sequencer.c
+++ sequencer.c
@@ -2655,6 +2655,7 @@ static int create_seq_dir(void)
 	if (file_exists(git_path_seq_dir())) {
 		error(_("a cherry-pick or revert is already in progress"));
 		advise(_("try \"git cherry-pick (--continue | --quit | --abort)\""));
+		advise(_("or  \"git revert (--continue | --quit | --abort)\""));
 		return -1;
 	} else if (mkdir(git_path_seq_dir(), 0777) < 0)
 		return error_errno(_("could not create sequencer directory '%s'"),
